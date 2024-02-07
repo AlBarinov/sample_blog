9d1c38807e8d4d2cbb26facb9316187a0d1b483e
AlBarinov <albarinov71@gmail.com>
2024-02-07 12:11:09 +0300

    Реализовал функцию удаления пользователя через его "личный кабинет". Также добавил редакто для администрации сайта.

--- sampleblog/main/urls.py
+++ sampleblog/main/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import index, about_page, BlogLoginView, profile, BlogLogoutView, ChangeUserInfoView, BlogPasswordChangeView, RegisterUserView
+from .views import index, about_page, BlogLoginView, profile, BlogLogoutView, ChangeUserInfoView, BlogPasswordChangeView, RegisterUserView, DeleteUserView
 
 
 app_name = 'main'
 urlpatterns = [
     path('accounts/logout/', BlogLogoutView.as_view(), name='logout'),
     path('accounts/password/change/', BlogPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/profile/delete/', DeleteUserView.as_view(), name='profile_delete'),
     path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/register/', RegisterUserView.as_view(), name='register'),
--- sampleblog/main/views.py
+++ sampleblog/main/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import UpdateView, CreateView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import logout
+from django.contrib import messages
 
 from .models import AdvUser
 from .forms import ChangeUserInfoForm, RegisterUserForm
@@ -42,6 +44,25 @@ class RegisterUserView(CreateView):
     template_name = 'main/register_user.html'
     form_class = RegisterUserForm
     success_url = reverse_lazy('main:profile')
+
+class DeleteUserView(LoginRequiredMixin, DeleteView):
+    model = AdvUser
+    template_name = 'main/delete_user.html'
+    success_url = reverse_lazy('main:index')
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        messages.add_message(request, messages.SUCCESS, 'Пользователь удален')
+        return super().post(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
 
 def index(request):
     return render(request, 'main/index.html')
